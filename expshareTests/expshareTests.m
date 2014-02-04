cfe56ef261c1364bf0c3ca8f0867c66342fc5173
ctf <tim.chen.86@gmail.com>
2014-02-04 20:37:50 +0800

    Initial commit from local git

---
+++ expshareTests/expshareTests.m
@@ -0,0 +1,34 @@
+//
+//  expshareTests.m
+//  expshareTests
+//
+//  Created by ctf on 04/02/2014.
+//  Copyright (c) 2014 ctf. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface expshareTests : XCTestCase
+
+@end
+
+@implementation expshareTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
