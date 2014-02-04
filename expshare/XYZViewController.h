fe4fb1f74b156ba7dd2b9818d3136af5e5154274
ctf <tim.chen.86@gmail.com>
2014-02-04 23:00:21 +0800

    just import

---
+++ expshare/XYZViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYZViewController.h
+//  expshare
+//
+//  Created by ctf on 04/02/2014.
+//  Copyright (c) 2014 ctf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZViewController : UITableViewController
+
+@end
